f3b6f27a646cb4e39d259549022f8f4eb3c83cc1
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2025-01-11 20:41:20 +1100

    added style to calculator container

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
 import style from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CalculatorContainer from './components/CalculatorContainer';
 import Display from './components/Display';
 import ButtonsContainer from './components/ButtonsContainer';
 
 function App() {
 	return (
 		<>
-			<div id="calculator" className={style.calculator}>
-				<Display />
-				<ButtonsContainer />
+			<div className={style.Container}>
+				<CalculatorContainer>
+					<Display />
+					<ButtonsContainer />
+				</CalculatorContainer>
 			</div>
 		</>
 	);
--- src/components/CalculatorContainer.jsx
+++ src/components/CalculatorContainer.jsx
@@ -1,3 +1,11 @@
-const CalculatorContainer = () => {};
+import style from './CalculatorContainer.module.css';
+
+const CalculatorContainer = (props) => {
+	return (
+		<div id="calculator" className={style.calculator}>
+			{props.children}
+		</div>
+	);
+};
 
 export default CalculatorContainer;
